3543a1ff3001629e2b90a3820d99fb0f039a74d7
Paul Petillot <petillotpaul@gmail.com>
2018-10-22 09:53:07 +0100

    gulp add and SASS

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,53 @@
-const gulp = require("gulp"); // Load Gulp!
-// Now that we've installed the uglify package we can require it:
-const uglify = require("gulp-uglify"),
-  rename = require("gulp-rename");
-const browserSync = require('browser-sync').create();
-const eslint = require('gulp-eslint');
+const gulp = require('gulp'),
+  prettyError = require('gulp-prettyerror'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('gulp-autoprefixer'),
+  rename = require('gulp-rename'),
+  cssnano = require('gulp-cssnano'),
+  uglify = require('gulp-uglify'),
+  eslint = require('gulp-eslint');
+  var browserSync = require('browser-sync').create();
+
 gulp.task("default", function() {
-  return gulp
-    .src("./js/*.js") // What files do we want gulp to consume?
-    .pipe(uglify()) // Call the uglify function on these files
-    .pipe(rename({ extname: ".min.js" })) // Rename the uglified file
-    .pipe(gulp.dest("./build/js")); // Where do we put the result?
+ return gulp
+   .src("./js/*.js")
+   .pipe(uglify())
+   .pipe(rename({ extname: ".min.js" }))
+   .pipe(gulp.dest("./build/js"));
 });
+
 gulp.task("browser-sync", function() {
-  browserSync.init({
-    server: {
-      baseDir: "./"
-    }
-  });
-  gulp
-    .watch(['index.html', 'build/css/*.css', 'build/js/*.js'])
-    .on('change', browserSync.reload);
-});
+   browserSync.init({
+     server: {
+       baseDir: "./"
+     }
+   });
+
+   gulp
+     .watch(['index.html', 'build/css/*.css', 'build/js/*.js'])
+     .on('change', browserSync.reload);
+ });
+
 gulp.task('lint', function() {
-  return gulp.src("./js/*.js").pipe(eslint(
-  ))
-  .pipe(eslint.format())
-  .pipe(eslint.failOnError());
+  return gulp.src("./js/*.js")
+
+      .pipe(eslint())
+      .pipe(eslint.format())
+      .pipe(eslint.failAfterError());
+});
+
+gulp.task("sass", function() {
+  return gulp
+    .src("./sass/style.scss")
+    .pipe(prettyError()) // ADD THIS LINE
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        browsers: ["last 2 versions"]
+      })
+    )
+    .pipe(gulp.dest("./build/css"))
+    .pipe(cssnano())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("./build/css"));
 });
--- js/main.js
+++ js/main.js
@@ -1,3 +1,59 @@
-var hello='hello';
-var world='world';
-console.log(hello + world);
+var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
+url += '?' + $.param({
+    'api-key': "82f12f3e4189492f86578cb2d4e6add0"
+   });
+var urlTechSection = ""
+urlTechSection += '?' + $.param({
+    'api-key': "82f12f3e4189492f86578cb2d4e6add0"
+   });
+var urlSportSection = ""
+urlSportSection += '?' + $.param({
+    'api-key': "82f12f3e4189492f86578cb2d4e6add0"
+   });
+var urlArtSection = ""
+urlArtSection += '?' + $.param({
+    'api-key': "82f12f3e4189492f86578cb2d4e6add0"
+    });
+var urlStyleSection = ""
+urlStyleSection += '?' + $.param({
+    'api-key': "82f12f3e4189492f86578cb2d4e6add0"
+    });
+
+
+//Make the request
+//For a single article console log the data
+//Put the data on the page
+$.ajax({
+    url: url,
+    method: 'GET',
+   }).done(function(data) {
+    console.log(data.results[0].abstract);
+    // const abstract = data.results[0].abstract;
+    console.log(data.results[0].multimedia[0]);
+    // const image = data.results[0].multimedia[0];
+    console.log(data.results[0].url);
+    // const url = data.results[0].url;
+    var $img = data.results[0].multimedia[0].url;
+    var $url = data.results[0].url;
+    var $description = data.results[0].abstract;
+    $('.articleImage').attr("src", $img);
+    $('.articleLink').attr("href", $url);
+    $('.articleDescription').attr("h2", $description);
+   }).fail(function(err) {
+    throw err;
+   });
+
+   
+
+
+//Create a loop for the articles
+
+
+//Listen for change for the select list
+
+//Filter the different request
+
+//Move the logo
+
+
+
