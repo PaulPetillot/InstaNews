33b3749213be4c4346bf3257b611cead0bbf73bf
Paul Petillot <petillotpaul@gmail.com>
2018-10-22 15:55:40 +0100

    Select and Filter Javascript

--- js/main.js
+++ js/main.js
@@ -1,49 +1,36 @@
-var url = "https://api.nytimes.com/svc/topstories/v2/home.json";
-url += '?' + $.param({
-    'api-key': "82f12f3e4189492f86578cb2d4e6add0"
-   });
-var urlTechSection = ""
-urlTechSection += '?' + $.param({
-    'api-key': "82f12f3e4189492f86578cb2d4e6add0"
-   });
-var urlSportSection = ""
-urlSportSection += '?' + $.param({
-    'api-key': "82f12f3e4189492f86578cb2d4e6add0"
-   });
-var urlArtSection = ""
-urlArtSection += '?' + $.param({
-    'api-key': "82f12f3e4189492f86578cb2d4e6add0"
-    });
-var urlStyleSection = ""
-urlStyleSection += '?' + $.param({
-    'api-key': "82f12f3e4189492f86578cb2d4e6add0"
-    });
+$('select').on('change', function(){
+    var url = "https://api.nytimes.com/svc/topstories/v2/"+$('select').val()+".json";
+    url += '?' + $.param({
+        'api-key': "82f12f3e4189492f86578cb2d4e6add0"
+       });
+    
+    //Make the request
+    //For a single article console log the data
+    //Put the data on the page
+    $.ajax({
+        url: url,
+        method: 'GET',
+       }).done(function(data) { 
+        $('.oneArticle').empty();
+        for(var i=0; i<12; i++){
+        console.log(data.results[i].abstract);
+        // const abstract = data.results[0].abstract;
+        console.log(data.results[i].multimedia[4]);
+        // const image = data.results[0].multimedia[0];
+        console.log(data.results[i].url);
+        // const url = data.results[0].url;
+        var $img = data.results[i].multimedia[4].url;
+        var $url = data.results[i].url;
+        var $description = data.results[i].abstract;
+        console.log(data);
+        $('.oneArticle').append('<a href="'+$url+'"class="articleLink"><img src="'+$img+'" class="articleImage"><h2 class="articleDescription">'+ $description+ '</h2></a>')
+        }
+        
+       }).fail(function(err) {
+        throw err;
+       });      
+})
 
-
-//Make the request
-//For a single article console log the data
-//Put the data on the page
-$.ajax({
-    url: url,
-    method: 'GET',
-   }).done(function(data) {
-    console.log(data.results[0].abstract);
-    // const abstract = data.results[0].abstract;
-    console.log(data.results[0].multimedia[0]);
-    // const image = data.results[0].multimedia[0];
-    console.log(data.results[0].url);
-    // const url = data.results[0].url;
-    var $img = data.results[0].multimedia[0].url;
-    var $url = data.results[0].url;
-    var $description = data.results[0].abstract;
-    $('.articleImage').attr("src", $img);
-    $('.articleLink').attr("href", $url);
-    $('.articleDescription').attr("h2", $description);
-   }).fail(function(err) {
-    throw err;
-   });
-
-   
 
 
 //Create a loop for the articles
